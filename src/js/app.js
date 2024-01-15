443b02d042f2391f2a1bf90fdaffba7ddd74263b
gangsoooha <happy95virus@gmail.com>
2024-01-15 21:46:20 +0900

    01_15

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import GUI from 'lil-gui';
-import { gsap } from 'gsap';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
+import { gsap } from 'gsap';
+import GUI from 'lil-gui';
 
 export default function () {
   /**
@@ -18,10 +20,19 @@ export default function () {
   let enteredMenu = null
   let newMenu = null
 
+
+  /** 연습용 */
+  const BLOOM_SCENE = 1;
+  const bloomLayer = new THREE.Layers()
+  bloomLayer.set(BLOOM_SCENE)
+
   const renderer = new THREE.WebGLRenderer({
     alpha: true,
   });
-  renderer.setClearColor(0x0a1245, 1);
+  // renderer.outputEncoding = new THREE.sRGBEncoding;
+  // renderer.toneMapping = THREE.ReinhardToneMapping;
+  renderer.setClearColor(0x282828, 1);
+  renderer.autoClear = false;
 
   const container = document.querySelector('#container');
   container.appendChild(renderer.domElement);
@@ -32,7 +43,9 @@ export default function () {
   };
 
 
-
+  /**
+   * threejs requiers
+   */
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
     75,
@@ -42,92 +55,116 @@ export default function () {
   );
 
 
+
+  /**
+   *  selectBloom 
+   */
+  const selectBloom = () =>{
+
+  }
+
+
+  /**
+   * post processing
+   */
+  const effectComposer = new EffectComposer(renderer);
+  const addPostEffects = () =>{
+    // effectComposer.renderToScreen = false
+    const renderpass = new RenderPass(scene, camera);
+    const unrealBloomPass = new UnrealBloomPass(
+      new THREE.Vector2(canvasSize.width / canvasSize.height)
+    )
+    unrealBloomPass.strength  = 0.2 // unrealBloomPass 파워
+    effectComposer.addPass(renderpass);
+    effectComposer.addPass(unrealBloomPass);
+
+  }
+
+
+
   /**
    *  lights
    */
-  const directionalLight =  new THREE.DirectionalLight('0#000000',0.2,100)
+  const directionalLight =  new THREE.DirectionalLight('0#000000',1,100)
+  // const directionalLight =  new THREE.AmbientLight('0#000000',0.2,100)
   directionalLight.position.set(10,10,10)
   scene.add(directionalLight);
-
+  
   /**
    * fontLoader
    */
-  const fontLoader = new FontLoader();
-  fontLoader.load('public/assets/fonts/Mukta_font.json', function (font){
-    const messages =['ABOUT','WORK','CONTACT']
-    const material = new THREE.MeshBasicMaterial({
-      color:'blue',
-      // transparent: true,
-      opacity: 1,
-    })
-    const fillmaterial = new THREE.MeshBasicMaterial({
-      color:'blue',
-      transparent: true,
-      opacity: 0.11,
-    })
+  const textloader = () => {
+    const fontLoader = new FontLoader();
+    fontLoader.load('public/assets/fonts/Mukta_font.json', function (font){
+      const messages =['ABOUT','WORK','CONTACT']
+      const material = new THREE.MeshBasicMaterial({
+        color:'blue',
+        // transparent: true,
+        opacity: 1,
+      })
+      const fillmaterial = new THREE.MeshBasicMaterial({
+        color:'blue',
+        transparent: true,
+        opacity: 0.11,
+      })
 
-    for(let i = 0; i < messages.length; i++){
-      const shapes = font.generateShapes( messages[i], 1.7 );
-      const cloneFill = fillmaterial.clone();
-      const geometry = new THREE.ShapeGeometry(shapes)
-      //get center
-      geometry.computeBoundingBox();
-      // const xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x ) ;
-      const xMid = - 5 ;
-      geometry.translate( xMid, 0, 0 );
-    
+      for(let i = 0; i < messages.length; i++){
+        const shapes = font.generateShapes( messages[i], 1.7 );
+        const cloneFill = fillmaterial.clone();
+        const geometry = new THREE.ShapeGeometry(shapes)
+        //get center
+        geometry.computeBoundingBox();
+        // const xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x ) ;
+        const xMid = - 5 ;
+        geometry.translate( xMid, 0, 0 );
       
-      const mesh = new THREE.Mesh(geometry,cloneFill);
-      mesh.position.y = i *2.3
-      mesh.position.z = 0.51
-      mesh.name = 'menu'
-      
-      scene.add(mesh)
+        
+        const mesh = new THREE.Mesh(geometry,cloneFill);
+        mesh.position.y = i *2.3
+        mesh.position.z = 0.51
+        mesh.name = 'menu'
+        
+        scene.add(mesh)
 
-      /**
-       * LineTextGeometry
-       */
-      const holeShapes = [];
-      
-
+        /**
+         * LineTextGeometry
+         */
 
 
-      for(let j = 0; j < shapes.length; j++){
-        const shape = shapes[j];
-        if ( shape.holes && shape.holes.length > 0 ) {
+        for(let j = 0; j < shapes.length; j++){
+          const shape = shapes[j];
+          if ( shape.holes && shape.holes.length > 0 ) {
 
-          for ( let k = 0; k < shape.holes.length; k++ ) {
+            for ( let k = 0; k < shape.holes.length; k++ ) {
 
-            const hole = shape.holes[ k ];
-            holeShapes.push( hole );
+              const hole = shape.holes[ k ];
+              holeShapes.push( hole );
+            }
           }
         }
-      }
 
-      shapes.push.apply( shapes, holeShapes );
-      const lineText = new THREE.Object3D();
-      for ( let j = 0; j < shapes.length; j ++ ) {
+        shapes.push.apply( shapes, holeShapes );
+        const lineText = new THREE.Object3D();
+        for ( let j = 0; j < shapes.length; j ++ ) {
 
-        const shape = shapes[ j ];
+          const shape = shapes[ j ];
 
-        const points = shape.getPoints();
-        const geometry = new THREE.BufferGeometry().setFromPoints( points );
-        geometry.translate( xMid, 0, 0 );
-        const lineMesh = new THREE.Line( geometry, material );
+          const points = shape.getPoints();
+          const geometry = new THREE.BufferGeometry().setFromPoints( points );
+          geometry.translate( xMid, 0, 0 );
+          const lineMesh = new THREE.Line( geometry, material );
 
-        geometry.computeBoundingBox();
+          geometry.computeBoundingBox();
 
-        lineMesh.position.y = i * 2.3
-        lineText.add( lineMesh );
-      }
-      scene.add( lineText );
-    }// fontLoading end
+          lineMesh.position.y = i * 2.3
+          lineText.add( lineMesh );
+        }
+        scene.add( lineText );
+      }// fontLoading end
 
-    
-
-    // scene.add(mesh)
-  })
-
+      // scene.add(mesh)
+    })// textloader end
+  }//textloader end
 
   /** library */
 
@@ -166,10 +203,58 @@ export default function () {
     return mesh
   }// createMenu end
   
+  /**
+   * createBox
+   */
+  const createBox = () => {
+
+
+
+    // const geometry = new THREE.BoxGeometry(3,3,3);
+    // const material = new THREE.MeshBasicMaterial({
+    //   color: 'red',
+    //   transparent: true,
+    //   opacity:.3,
+    // })
+    // const mesh = new THREE.Mesh(geometry,material)
+    // const cloen = mesh.clone()
+
+    // cloen.layers.enable(BLOOM_SCENE)
+    // cloen.position.set(0,4,0)
+    // scene.add(mesh,cloen)
+    // // return mesh 
+    const geometry = new THREE.IcosahedronGeometry( 1, 15 );
+
+    for ( let i = 0; i < 50; i ++ ) {
+
+      const color = new THREE.Color();
+      color.setHSL( Math.random(), 0.7, Math.random() * 0.2 + 0.05 );
+
+      const material = new THREE.MeshBasicMaterial( { color: color } );
+      const sphere = new THREE.Mesh( geometry, material );
+      sphere.position.x = Math.random() * 10 - 5;
+      sphere.position.y = Math.random() * 10 - 5;
+      sphere.position.z = Math.random() * 10 - 5;
+      sphere.position.normalize().multiplyScalar( Math.random() * 4.0 + 2.0 );
+      sphere.scale.setScalar( Math.random() * Math.random() + 0.5 );
+      scene.add( sphere );
+
+      if ( Math.random() < 0.25 ) sphere.layers.enable( BLOOM_SCENE );
+      console.log(sphere.layers)
+    }
+
+  }
+
+
+
+
+
   /** create */
   const create = () => {
-    const menuList = createMenu();
-    scene.add(menuList);
+    createBox()
+    // const menuList = createMenu();
+    // scene.add(menuList);
+      
   };// create end
 
   const resize = () => {
@@ -181,6 +266,8 @@ export default function () {
 
     renderer.setSize(canvasSize.width, canvasSize.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    effectComposer.setSize(canvasSize.width, canvasSize.height);
+    
   };// resize end
 
   const addEvent = () => {
@@ -217,7 +304,7 @@ export default function () {
       }//menuCheck end
   }
 
-  meunCheck()
+  // meunCheck()
  }//getRaycaster end
 
  /**
@@ -254,10 +341,12 @@ export default function () {
  }//menu check end
 
   const draw = ( orbitControl) => {
+    renderer.clear();
+
 
     orbitControl.update();
-    renderer.render(scene, camera);
-
+    // renderer.render(scene, camera);
+    effectComposer.render()
     requestAnimationFrame(() => {
       draw(orbitControl);
     });
@@ -267,6 +356,7 @@ export default function () {
 
 
   const initialize = () => {
+    addPostEffects()
     create();
     const orbitControl = orbitControls()
     addEvent();
